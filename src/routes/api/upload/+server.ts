7d646cc3731553a826066fe56e1fe51c1112fd39
Michael Esteban <mickel13@gmail.com>
2024-09-20 16:18:00 +1000

    update prompt to group similar text

--- src/routes/api/upload/+server.ts
+++ src/routes/api/upload/+server.ts
@@ -20,56 +20,56 @@ export async function POST({ request }) {
 	const fileManager = new GoogleAIFileManager(GOOGLE_API_KEY);
 
 	// Upload file
-	// try {
-	// 	tempFileHandle = await tempFile({ postfix: `.${file.name.split('.').pop()}` });
+	try {
+		tempFileHandle = await tempFile({ postfix: `.${file.name.split('.').pop()}` });
 
-	// 	const arrayBuffer = await file.arrayBuffer();
-	// 	await writeFile(tempFileHandle.path, Buffer.from(arrayBuffer));
-	// 	uploadResult = await fileManager.uploadFile(tempFileHandle.path, {
-	// 		mimeType: file.type
-	// 	});
-	// } catch (error) {
-	// 	console.error(error);
-	// 	return new Response('Error uploading file', { status: 500 });
-	// } finally {
-	// 	if (tempFileHandle) {
-	// 		tempFileHandle.cleanup();
-	// 	}
-	// }
+		const arrayBuffer = await file.arrayBuffer();
+		await writeFile(tempFileHandle.path, Buffer.from(arrayBuffer));
+		uploadResult = await fileManager.uploadFile(tempFileHandle.path, {
+			mimeType: file.type
+		});
+	} catch (error) {
+		console.error(error);
+		return new Response('Error uploading file', { status: 500 });
+	} finally {
+		if (tempFileHandle) {
+			tempFileHandle.cleanup();
+		}
+	}
 
-	// console.log(uploadResult);
+	console.log(uploadResult);
 
 	// Generate transcript
 	const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 
 	const model = genAI.getGenerativeModel({
-		model: 'gemini-1.5-flash',
+		model: 'gemini-1.5-flash-exp-0827',
 		generationConfig: { responseMimeType: 'application/json' }
 	});
 
 	// Check that the file has been processed
-	// let uploadedFile = await fileManager.getFile(uploadResult.file.name);
+	let uploadedFile = await fileManager.getFile(uploadResult.file.name);
 
-	// while (uploadedFile.state === FileState.PROCESSING) {
-	// 	console.log('File is processing...');
-	// 	await new Promise((resolve) => setTimeout(resolve, 5000));
-	// 	uploadedFile = await fileManager.getFile(uploadResult.file.name);
-	// }
+	while (uploadedFile.state === FileState.PROCESSING) {
+		console.log('File is processing...');
+		await new Promise((resolve) => setTimeout(resolve, 5000));
+		uploadedFile = await fileManager.getFile(uploadResult.file.name);
+	}
 
-	// if (uploadedFile.state === FileState.FAILED) {
-	// 	return new Response('Error processing file', { status: 500 });
-	// }
+	if (uploadedFile.state === FileState.FAILED) {
+		return new Response('Error processing file', { status: 500 });
+	}
 
 	const result = await model.generateContentStream([
 		{
 			fileData: {
 				mimeType: file.type,
-				// fileUri: uploadResult.file.uri
-				fileUri: 'https://generativelanguage.googleapis.com/v1beta/files/f37t0wnzuvds'
+				fileUri: uploadResult.file.uri
+				// fileUri: 'https://generativelanguage.googleapis.com/v1beta/files/wbdi2xn3v0bp'
 			}
 		},
 		{
-			text: `Generate a transcript for this file. Always use the format mm:ss for the time. Respond with the transcript in the form of this JSON schema: 
+			text: `Generate a transcript for this file. Always use the format mm:ss for the time. Group similar text together rather than timestamping every line. Respond with the transcript in the form of this JSON schema:
      [{"timestamp": "00:00", "speaker": "Speaker 1", "text": "Today I will be talking about the importance of AI in the modern world."},{"timestamp": "01:00", "speaker": "Speaker 1", "text": "Has AI has revolutionized the way we live and work?"}]`
 		}
 	]);
