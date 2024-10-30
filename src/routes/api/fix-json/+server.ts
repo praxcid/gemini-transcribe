c0d2b9a1f8692d36523c85c9c9f248b54822a26b
Michael Esteban <mickel13@gmail.com>
2024-10-30 20:54:00 +1000

    use jsonrepair package instead of gemini

--- src/routes/api/fix-json/+server.ts
+++ src/routes/api/fix-json/+server.ts
@@ -1,24 +1,7 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import { env } from '$env/dynamic/private';
 import { json } from '@sveltejs/kit';
-import { safetySettings } from '$lib/index';
+import { jsonrepair } from 'jsonrepair';
 
 export async function POST({ request }) {
 	const buffer = await request.text();
-
-	const genAI = new GoogleGenerativeAI(env.GOOGLE_API_KEY);
-
-	const model = genAI.getGenerativeModel({
-		model: 'gemini-1.5-flash-exp-0827',
-		safetySettings,
-		generationConfig: { responseMimeType: 'application/json' }
-	});
-
-	const prompt = `Fix this poorly formatted JSON buffer. Return an object with a key of "formattedJSON". \n\n BUFFER: ${buffer}`;
-
-	const result = await model.generateContent(prompt);
-
-	const { formattedJSON } = JSON.parse(result.response.text());
-
-	return json({ formattedJSON });
+	return json({ formattedJSON: JSON.parse(jsonrepair(buffer)) });
 }
